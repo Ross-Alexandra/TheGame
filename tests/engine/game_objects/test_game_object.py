7af237c79b44af87ad136765fd4336697c280019
Ross Alexandra <ross3834@uvic.ca>
2019-01-17 11:48:59 -0800

    GH-14 - Added unit tests for game_object.

--- tests/engine/game_objects/test_game_object.py
+++ tests/engine/game_objects/test_game_object.py
@@ -1,3 +1,7 @@
+from unittest.mock import Mock
+
+import pytest
+
 from thegame.engine.game_objects import GameObject
 
 
@@ -15,3 +19,63 @@ def test_initialize_game_object_without_animation_set_it_to_none():
 
     assert go.sprite_location == "sprite.png"
     assert go.animation is None
+
+
+def test_clone_produces_object_with_same_sprite_info():
+
+    go = GameObject("sprite.png")
+    clone = go.clone()
+
+    assert clone.sprite_location == go.sprite_location
+    assert clone.animation == go.animation
+
+
+def test_the_registered_sprite_can_be_loaded_and_unloaded():
+
+    go = GameObject("sprite.png")
+    sprite_mock = Mock()
+    go.register_loaded_sprite(sprite_mock)
+
+    assert go._loaded_sprite is sprite_mock
+
+    go.deregister_loaded_sprite()
+
+    assert go._loaded_sprite is None
+
+
+def test_get_sprite_returns_sprite():
+
+    go = GameObject("sprite.png")
+    sprite_mock = Mock()
+    go.register_loaded_sprite(sprite_mock)
+
+    assert go.get_sprite() is sprite_mock
+
+
+def test_get_sprite_on_spriteless_object_throws_value_error():
+
+    go = GameObject("sprite.png")
+
+    with pytest.raises(ValueError):
+        go.get_sprite()
+
+
+def test_set_sprite_position_sets_position_correctly():
+
+    go = GameObject("sprite.png")
+    sprite_mock = Mock()
+    go.register_loaded_sprite(sprite_mock)
+
+    go.set_sprite_position(pos_x=5, pos_y=4)
+
+    assert sprite_mock.image.get_rect.called
+    assert sprite_mock.rect.x == 5
+    assert sprite_mock.rect.y == 4
+
+
+def test_set_sprite_position_on_spriteless_object_throws_value_error():
+
+    go = GameObject("sprite.png")
+
+    with pytest.raises(ValueError):
+        go.set_sprite_position(pos_x=5, pos_y=4)
